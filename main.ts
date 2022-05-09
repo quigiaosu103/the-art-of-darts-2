27a7c635a3bfbce41b878229bea9f95b2f8860ee
quigiaosu103 <93850311+quigiaosu103@users.noreply.github.com>
2022-05-09 11:59:50 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+namespace SpriteKind {
+    export const Gas = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    darts = [assets.image`Dart1`, assets.image`Blue Rocket`, assets.image`Dart2`]
+    projectile = sprites.createProjectileFromSprite(darts._pickRandom(), mySprite, 0, -150)
+    projectile.startEffect(effects.ashes)
+})
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Gas, function (sprite, otherSprite) {
+    statusbar.value = 100
+    otherSprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy(effects.fire, 100)
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+    if (info.score() == 10) {
+        info.changeScoreBy(5)
+        mySprite.sayText("+5 Level-Up Bonus!", 2000, false)
+        enemySpeed = 70
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 200)
+    scene.cameraShake(4, 500)
+})
+let myEnemy: Sprite = null
+let myFuel: Sprite = null
+let projectile: Sprite = null
+let darts: Image[] = []
+let enemySpeed = 0
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`Galaxy`)
+scroller.scrollBackgroundWithSpeed(0, 10)
+mySprite = sprites.create(assets.image`Rocket`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+animation.runImageAnimation(
+mySprite,
+assets.animation`Flying Rocket`,
+100,
+true
+)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.attachToSprite(mySprite, -25, 0)
+enemySpeed = 50
+game.onUpdateInterval(5000, function () {
+    myFuel = sprites.createProjectileFromSide(assets.image`Fuel`, 0, 50)
+    myFuel.x = randint(5, 155)
+    myFuel.setKind(SpriteKind.Gas)
+})
+game.onUpdateInterval(2000, function () {
+    myEnemy = sprites.createProjectileFromSide(assets.image`Spider`, 0, enemySpeed)
+    myEnemy.x = randint(5, 155)
+    myEnemy.setKind(SpriteKind.Enemy)
+    animation.runImageAnimation(
+    myEnemy,
+    assets.animation`Flying Spider`,
+    100,
+    true
+    )
+})
+game.onUpdateInterval(300, function () {
+    statusbar.value += -1
+})
